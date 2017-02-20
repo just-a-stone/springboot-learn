25f08f94595be9712bf2f9746a8269f22164340c
lupf <lupf@oriental-finance.com>
2017-02-20 18:25:51 +0800

    first commited

---
+++ web/src/main/java/com/example/web/controller/HelloController.java
@@ -0,0 +1,37 @@
+package com.example.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lupf on 2017/2/20.
+ */
+//@RestController
+@Controller
+@RequestMapping("hello")
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("say")
+    public Object say() {
+        Map map = new HashMap();
+        map.put("aaa", "hello world!");
+
+        return map;
+    }
+
+    @RequestMapping("thy")
+    public String thy(Model model) {
+        Map map = new HashMap();
+        map.put("say", "hello world!");
+
+        model.addAttribute("person", map);
+
+        return "index";
+    }
+}
